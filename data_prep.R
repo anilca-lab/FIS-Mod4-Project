bd862c31d015f804834f8c3311b4e7401f83c54a
Anil Onal <anilca@gmail.com>
2019-11-30 11:10:57 -0500

    aggregated complaints

---
+++ data_prep.R
@@ -0,0 +1,21 @@
+# This script prepares the data files for processing in Python
+# while reducing their size.
+rm(list = ls())
+library(dplyr)
+library(stringr)
+complaints_df <- read_csv('/Users/flatironschol/FIS-Projects/Module4/data/NYPD_Complaint_Data_Historic.csv')
+offense_types_df <- complaints_df %>% select(KY_CD, OFNS_DESC, PD_CD, PD_DESC) %>% distinct()
+borough_df <- complaints_df %>% select(BORO_NM, ADDR_PCT_CD) %>% distinct()
+complaints_df <- complaints_df %>% select(CMPLNT_NUM, RPT_DT, ADDR_PCT_CD, KY_CD)
+complaints_df <- complaints_df %>% mutate(YEAR = as.numeric(str_sub(RPT_DT,-4,-1)))
+complaints_df <- complaints_df %>% select(-RPT_DT) %>% group_by(YEAR, ADDR_PCT_CD, KY_CD) %>% summarise(CMPLNTS = n())
+write.csv(complaints_df, file='/Users/flatironschol/FIS-Projects/Module4/FIS-Mod4-Project/data/complaints_df.csv', row.names=FALSE)
+write.csv(offense_types_df, file='/Users/flatironschol/FIS-Projects/Module4/FIS-Mod4-Project/data/offense_types_df.csv', row.names=FALSE)
+write.csv(borough_df, file='/Users/flatironschol/FIS-Projects/Module4/FIS-Mod4-Project/data/borough_df.csv', row.names=FALSE)
+
+rm(list = ls())
+library(dplyr)
+library(stringr)
+arrests_df <- read_csv('/Users/flatironschol/FIS-Projects/Module4/data/NYPD_Arrests_Data__Historic_.csv')
+arrests_df <- arrests_df %>% mutate(YEAR = as.numeric(str_sub(ARREST_DATE,-4,-1)), MONTH = as.numeric(str_sub(ARREST_DATE,-7,-6))) %>% select(ARREST_KEY, YEAR, MONTH, KY_CD, ARREST_PRECINCT, AGE_GROUP:PERP_RACE)
+write.csv(arrests_df, file='/Users/flatironschol/FIS-Projects/Module4/FIS-Mod4-Project/data/arrests_df.csv', row.names=FALSE)
